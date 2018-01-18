7dcb4e24877c22b829d2cdbb09625f96072bea90
Nick Yulman <nyulman@kickstarter.com>
2018-01-18 16:49:22 -0500

    add sorting

--- sketch.js
+++ sketch.js
@@ -1,9 +1,16 @@
 var table;
 var section = "Design & Tech";
 var articles = [];
+var selecters = [];
 var titleOS = 20;
 var vSpacing = 20;
 var topPadding = 200;
+var selSpacing = 100;
+var catSel;
+var displayedX = 100;
+var nondisplayedX = 600;
+
+var speed = 10;
 
 
 function preload() {
@@ -11,7 +18,7 @@ function preload() {
   //and has a header specifying the columns labels
   //table = loadTable('data/ksrea.csv', 'csv', 'header');
   //the file can be remote
-  table = loadTable("https://docs.google.com/spreadsheets/d/e/2PACX-1vRi9ipGYonyrz7R0K9qGxOrchoK7RomUaHX84ycaoauhNKyGqNLZ2zb2pSFpLRwh5JS4P6O53dMxm2j/pub?output=csv", "csv", "header");
+  table = loadTable("https://docs.google.com/spreadsheets/d/e/2PACX-1vRp9CDCJKJvPug_GcCjOf2YmDlh57-M6dh3DdhfKyYEqOSunPLGYFTuqV2UoHLRFwgkmcMX_wy3aKiz/pub?output=csv", "csv", "header");
 }
 
 function setup() {
@@ -27,11 +34,12 @@ function setup() {
   // print(table.getColumnCount() + ' total columns in table');
   //print(table.getColumn('name'));
 
+catSel = new Select(100,220);
+
   //cycle through the table
   for (var r = 0; r < table.getRowCount(); r++){
     for (var c = 0; c < table.getColumnCount(); c++) {
       //print(table.getString(r, c));
-
 
 switch(c) {
     case 0:
@@ -48,10 +56,24 @@ switch(c) {
     }
         break;
 
-}
-}
-}
+    case 2: //category
+    {
+    articles[r].category = table.getString(r, c);
+    //print(articles[r].category);
+    catSel.addOption(articles[r].category);
 
+    }
+        break;
+
+    case 3: {
+    articles[r].section = table.getString(r, c);
+
+    break;
+
+}
+}
+}
+}
 }
 
 function draw() {
@@ -60,14 +82,64 @@ function draw() {
   articles[r].display();
   articles[r].move();
   }
+  catSel.display();
+filterSelection();
 }
 
-function mousePressed() {
-
+function mouseReleased() {
   for(var r=0; r < articles.length; r++){
-  articles[r].tX+=random(-10,10);
+  //articles[r].tX+=random(-10,10);
   }
+  print(catSel.selectedItem);
+  //print(catSel.sel.value());
 
+}
+
+
+filterSelection = function(){
+for(var i=0; i<articles.length; i++){
+  if(articles[i].category == catSel.selectedItem){
+    articles[i].selected=true;
+    //print(articles[i].title+" = "+ articles[i].selected);
+  }
+  else articles[i].selected=false;
+  //print(articles[i].title+" = "+ articles[i].selected);
+}
+}
+
+// selecter class
+function Select(_x,_y){
+this.x = _x;
+this.y = _y;
+this.options = [];
+this.sel = createSelect();
+this.selectedItem = "all";
+//this.sel.changed(this.mySelectEvent());
+
+this.addOption = function(_o){
+this.options.push("all");
+found = false;
+for(var i=0; i<this.options.length; i++){
+if(this.options[i]==_o){
+  print("already have"+_o);
+  found=true;
+}
+if(i==this.options.length-1 && found==false){
+  this.options.push(_o);
+  this.sel.option(_o);
+    print("added"+_o);
+  }
+}
+}
+
+// this.mySelectEvent = function(){
+//
+// }
+
+this.display =function(){
+this.sel.position(this.x,this.y);
+this.selectedItem = this.sel.value();
+}
 }
 
 
@@ -75,13 +147,13 @@ function mousePressed() {
 function Article(_c,_r) {
   // this.x = random(width);
   // this.y = random(height);
-  this.x = 100;
+  this.x = displayedX;
   this.y = 200+vSpacing*_r;
   this.tX = this.x;
   this.tY = this.y;
   this.dotCol = color(3, 71, 82,random(50,255));
   this.diameter = random(5, 10);
-  this.speed = .5;
+  this.speed = speed;
   this.title; //0
   this.link; //1
   this.category; //2
@@ -91,35 +163,43 @@ function Article(_c,_r) {
   this.month;
   this.year;
   this.tLink;
+  this.selected = true;
+
 
   this.createLink=function(){
      this.tLink = createA(this.link, "<div style='color:black; font-family: sans-serif;'>"+this.title+ "</div>", "_blank");
   };
 
-
   this.move = function() {
     if(this.x>this.tX){
       this.x-=this.speed;
+      this.speed = abs(this.x-this.tX)/speed;
     }
     if(this.y>this.tY){
       this.y-=this.speed;
+      this.speed = abs(this.y-this.tY)/speed;
     }
     if(this.x<this.tX){
       this.x+=this.speed;
+      this.speed = abs(this.x-this.tX)/speed;
     }
     if(this.y<this.tY){
       this.y+=this.speed;
+      this.speed = abs(this.y-this.tY)/speed;
     }
   };
 
   this.display = function() {
     noStroke();
     fill(this.dotCol);
-    ellipse(this.x,this.y+6, this.diameter);
     this.tLink.position(this.x+titleOS,this.y+topPadding);
+    ellipse(this.x,this.y+6, this.diameter);
 
+    if(this.selected){
+    this.tX=displayedX;
   }
-
+  else this.tX= nondisplayedX;
+  }
 };
 
 
